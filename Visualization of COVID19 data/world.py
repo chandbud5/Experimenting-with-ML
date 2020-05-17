e301a44c978930cf8607723f54ddcc402273a3c9
Chand Bud <chandbud5@gmail.com>
2020-05-17 11:48:36 +0530

    Add files via upload

--- Visualization of COVID19 data/world.py
+++ Visualization of COVID19 data/world.py
@@ -8,26 +8,26 @@ dfw = df.loc[df['location']=='World']
 dt = dfw['date'].values
 y = dfw['total_cases'].values
 plt.plot(dt,y,'r--')
-plt.xticks(np.arange(0, 138, 10), rotation=90)
+plt.xticks(np.arange(0, 138, 10), rotation=-90)
 plt.title("Total cases in World")
-plt.savefig("Total-cases-in-World.png",dpi=450)
+plt.savefig("Total-cases-in-World.png",dpi=200)
 plt.show()
 
 plt.plot(dfw['date'], dfw['new_cases'], '--')
-plt.xticks(np.arange(0, 138, 10), rotation=90)
+plt.xticks(np.arange(0, 138, 10), rotation=-90)
 plt.title("New cases in World")
-plt.savefig("New-cases-in-World.png",dpi=450)
+plt.savefig("New-cases-in-World.png",dpi=200)
 plt.show()
 
 dfi = df.loc[df['location']=="India"]
-plt.plot(dfi['date'], dfi['total_cases'],'--')
-plt.xticks(np.arange(0, 138, 10), rotation=90)
+plt.plot(dfi['date'], dfi['total_cases'],'r--')
+plt.xticks(np.arange(0, 138, 10), rotation=-90)
 plt.title("Total cases in India")
-plt.savefig("Total-cases-in-India.png",dpi=450)
+plt.savefig("Total-cases-in-India.png",dpi=200)
 plt.show()
 
 plt.plot(dfi['date'], dfi['new_cases'], '--')
-plt.xticks(np.arange(0, 138, 10), rotation=90)
+plt.xticks(np.arange(0, 138, 10), rotation=-90)
 plt.title("New cases in India")
-plt.savefig("New-cases-in-India.png",dpi=450)
+plt.savefig("New-cases-in-India.png",dpi=200)
 plt.show()
